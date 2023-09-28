ba8f581c846da4bab4242fd9eb74189bec5e1379
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-28 18:28:10 +0200

    RegisterComponent: Email and Pattern Validations

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from 'src/app/shared/validators/validators';
+import * as customValidators from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required, cantBeStrider]],
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(customValidators.firstNameAndLastnamePattern),
+      ],
+    ],
+    email: [
+      '',
+      [Validators.required, Validators.pattern(customValidators.emailPattern)],
+    ],
+    username: ['', [Validators.required, customValidators.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     passwordConfirm: ['', [Validators.required]],
   });
