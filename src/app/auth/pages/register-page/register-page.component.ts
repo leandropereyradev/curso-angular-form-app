6480d0a6332e27aea9535576181b9d540df26dfb
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-28 17:47:33 +0200

    RefisterComponent: Conecting FormBuilder with HTML form

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    passwordConfirm: ['', [Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  isValid(field: string) {
+    // TODO: desde servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
