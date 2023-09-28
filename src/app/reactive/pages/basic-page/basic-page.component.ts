eac69e48ed25c3e332f27b6be5742749566e3d82
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-28 11:29:22 +0200

    BasciPageComponent: Validators

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -13,14 +18,16 @@ export class BasicPageComponent {
   // });
 
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
   constructor(private fb: FormBuilder) {}
 
   onSave(): void {
+    if (this.myForm.invalid) return;
+
     console.log(this.myForm.value);
   }
 }
