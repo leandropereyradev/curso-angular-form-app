751401cc3957f912d9a8db151066bd9e4e81474d
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-28 19:47:29 +0200

    RegisterComponent: Verify that two fields are equal

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -7,26 +7,37 @@ import { ValidatorsService } from 'src/app/shared/service/validators.service';
   templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
-  public myForm: FormGroup = this.fb.group({
-    name: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern(this.validatorService.firstNameAndLastnamePattern),
+  public myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.firstNameAndLastnamePattern),
+        ],
       ],
-    ],
-    email: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern(this.validatorService.emailPattern),
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.emailPattern),
+        ],
+        [this.emailValidatorService],
       ],
-      [this.emailValidatorService],
-    ],
-    username: ['', [Validators.required, this.validatorService.cantBeStrider]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    passwordConfirm: ['', [Validators.required]],
-  });
+      username: [
+        '',
+        [Validators.required, this.validatorService.cantBeStrider],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordConfirm: ['', [Validators.required]],
+    },
+    // Estos validadores pasan por TODO el formulario
+    {
+      validators: [
+        this.validatorService.passwordCompare('password', 'passwordConfirm'),
+      ],
+    }
+  );
 
   constructor(
     private fb: FormBuilder,
--- src/app/shared/service/validators.service.ts
+++ src/app/shared/service/validators.service.ts
@@ -21,4 +21,20 @@ export class ValidatorsService {
   public isValidField(form: FormGroup, field: string) {
     return form.controls[field].errors && form.controls[field].touched;
   }
+
+  public passwordCompare(field1: string, field2: string) {
+    return (formGroup: FormGroup): ValidationErrors | null => {
+      const value1 = formGroup.get(field1)?.value;
+      const value2 = formGroup.get(field2)?.value;
+
+      if (value1 !== value2) {
+        formGroup.get(field2)?.setErrors({ notEqual: true });
+        return { notEqual: true };
+      }
+
+      formGroup.get(field2)?.setErrors(null);
+
+      return null;
+    };
+  }
 }
